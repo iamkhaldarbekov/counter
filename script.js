28d2fd7fa4585debd8ae5eebe3bbae2a491b4ce7
Shakhruz <110553720+ksh4kh@users.noreply.github.com>
2023-11-04 09:52:20 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,20 +4,22 @@ function count() {
     let rankCount = 0
     const el = document.querySelector('.counter')
     const rank = document.querySelector('.rank span')
+    const body = document.querySelector('body')
 
-    let interval = setInterval(() => {
-        if (rankCount == 4) {
-            el.innerHTML = 'The End!'
-            clearInterval(interval)
-        }
-        
+    body.addEventListener('click', () => {
+        value++
         if (value == 60) {
             value = 0
             rankCount++
         }
+
+        if (rankCount == 4) {
+            el.innerHTML = 'THE END!'
+            body.removeEventListener('click')
+            return;
+        }
         el.innerHTML = value
         rank.innerHTML = ranks[rankCount]
-        value++
-    }, 1000)
+    })
 }
 count()
